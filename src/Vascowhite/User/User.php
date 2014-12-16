3475f4582e2e256e45901c39c5287468b281e9c1
Paul White <paul@vascowhite.co.uk>
2014-12-16 12:23:16 +0800

    Went back to CredentialChecker

--- src/Vascowhite/User/User.php
+++ src/Vascowhite/User/User.php
@@ -56,7 +56,7 @@ class User
     }
 
     /**
-     * @param CredentialChecker|CredentialChecker $credentialChecker
+     * @param CredentialChecker $credentialChecker
      * @param array $credentials
      * @return bool
      */
